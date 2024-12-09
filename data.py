a17e0d01c96a6d1a23026e4e9e7521062d170563
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2024-12-09 11:36:24 +0300

    load data updated

--- data.py
+++ data.py
@@ -16,6 +16,6 @@ def load_data():
 
     y = y.map(label_dict)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
     return X_train, X_test, y_train, y_test, label_dict
